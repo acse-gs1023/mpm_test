1f78e2939b7e4dbfc0ffbadd639ac82a98ba5cdb
acse-gs1023 <gs1023@ic.ac.uk>
2023-10-16 19:47:52 +0100

    aa

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='mpm_test',  # Name of package, required
     version='1.0.0',  # Version number, required
-    packages=['hello'],  # directories to install, required
+    packages=['mpm_test'],  # directories to install, required
     # One-line description or tagline of what your project does
     description='A sample implementation',  # Optional
     author='Garv Sharma',  # Optional
